cd01c86c537b0c695938aec2e2a8dbfdac2d5547
Mike <62350021+mmcm77@users.noreply.github.com>
2024-03-09 13:32:25 -0500

    Content updates
    Removing "Under construction" page and replacing with basic content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,15 @@ export default function Home() {
         <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <div className="mx-auto max-w-xl text-center">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
-              Under Construction
+              Mike McMillan
             </h1>
 
-            <p className="mt-4 sm:text-xl/relaxed">Working on some stuff 🏗️</p>
+            <p className="mt-4 sm:text-xl/relaxed">Product manager 🏗️</p>
+
+            <p className="mt-4 sm:text-l/relaxed">
+              Product leader with a decade of experience across Product
+              Management, Strategy and GTM.
+            </p>
           </div>
         </div>
       </section>
