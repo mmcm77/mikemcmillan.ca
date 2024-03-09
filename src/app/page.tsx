de3f9e30427e13e8716f2a32a83a27cbef2843ab
Mike <62350021+mmcm77@users.noreply.github.com>
2024-03-09 14:38:03 -0500

    update next and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
-import Image from "next/image";
+import Links from "@/components/LinksSection";
 
 export default function Home() {
   return (
     <main>
-      <section className="bg-black">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-          <div className="mx-auto max-w-xl text-center">
+      <section className="bg-black text-white mt-36">
+        {" "}
+        <div className="mx-auto max-w-screen-xl px-4 py-36">
+          <header className="mx-auto max-w-xl text-center">
+            {" "}
             <h1 className="text-3xl font-extrabold sm:text-5xl">
               Mike McMillan
             </h1>
-
-            <p className="mt-4 sm:text-xl/relaxed">Product manager 🏗️</p>
-
-            <p className="mt-4 sm:text-l/relaxed">
+            <div className="mt-4 sm:text-xl/relaxed">Product manager 🏗️</div>
+            <div className="mt-4 sm:text-l/relaxed">
               Product leader with a decade of experience across Product
               Management, Strategy and GTM.
-            </p>
-          </div>
+            </div>
+          </header>
         </div>
+        <Links />
       </section>
     </main>
   );
---
+++ src/components/LinksSection.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+const LinksSection: React.FC = () => {
+  return (
+    <section className="py-2">
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-medium mb-4 text-center">
+          Let's Connect!
+        </h2>
+        <ul className="flex sm:flex-row justify-center gap-6">
+          <li>
+            <Link href="https://www.linkedin.com/in/mmcmillan/">
+              <div className="text-gray-300 hover:underline">LinkedIn</div>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/mmcm77">
+              <p className="text-gray-300 hover:underline">GitHub</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="mailto:mikemcmillan55@gmail.com">
+              <p className="text-gray-300 hover:underline">Email</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default LinksSection;
